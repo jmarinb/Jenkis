be1157bac5ac916fd32c141e9c43e6f20401fd99
Julio Marin Baltazar <1463387@SOAM.TCS.com>
2018-03-28 12:24:48 -0600

    new commit

---
+++ src/main/java/Julio_Group/MavenExamples/App.java
@@ -0,0 +1,13 @@
+package Julio_Group.MavenExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
